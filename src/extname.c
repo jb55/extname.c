ffaf169ec54fe0942d055fdb9fd051cbfa3d6b47
William Casarin <william@casarin.me>
2014-02-16 16:00:06 -0800

    Simplify

--- src/extname.c
+++ src/extname.c
@@ -5,14 +5,6 @@
 const char *extname(const char *filename)
 {
   const char *c = filename;
-
-  // skip leading ./ if it exists
-  if (*c != '\0')
-  if (*c++ == '.')
-  if (*c != '\0')
-  if (*c++ == '/')
-    filename = c;
-
   const char *slash = strrchr(filename, '/');
   const char *loc = strrchr(slash? slash : filename, '.');
   return loc? loc : "";
